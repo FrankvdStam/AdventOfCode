93634c698ae2a9874e1e094bb40ebafbf47904ea
FrankvdStam <frank_stam5@hotmail.com>
2018-12-21 15:44:40 +0100

    init

---
+++ Day2/Program.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var split = (input.Split('\n')).ToList();
+            List<string> sanitized = new List<string>();
+            foreach (string s in split)
+            {
+                sanitized.Add(s.Replace("\r", ""));
+            }
+            //ProblemOne(sanitized);
+            ProblemTwo(sanitized);
+
+            Console.ReadKey();
+        }
+
+        static void ProblemOne(List<string> input)
+        {
+            int twoLetterCount = 0;
+            int threeLetterCount = 0;
+
+            foreach (string s in input)
+            {
+                if (HasExactLetterCount(s, 2))
+                {
+                    twoLetterCount++;
+                }
+
+                if (HasExactLetterCount(s, 3))
+                {
+                    threeLetterCount++;
+                }
+            }
+
+            int checksum = twoLetterCount * threeLetterCount;
+            Console.WriteLine(checksum);
+        }
+
+        private static bool HasExactLetterCount(string input, int letterCount)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (charCount.ContainsKey(input[i]))
+                {
+                    charCount[input[i]]++;
+                }
+                else
+                {
+                    charCount[input[i]] = 1;
+                }
+            }
+
+            return charCount.ContainsValue(letterCount);
+        }
+
+
+        private static void ProblemTwo(List<string> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input.Count; j++)
+                {
+                    //Don't compare with self.
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (HaveOneCharDifference(input[i], input[j]))
+                    {
+                        Console.WriteLine(input[i]);
+                        Console.WriteLine(input[j]);
+                    }
+                }
+            }
+        }
+
+        private static bool HaveOneCharDifference(string first, string second)
+        {
+            bool firstInequallity = true;
+            if (first.Length != second.Length)
+            {
+                Console.WriteLine("length not equal");
+            }
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    if (firstInequallity)
+                    {
+                        firstInequallity = false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        private static string input = @"xdmgyjkpruszabaqwficevtjeo
+xdmgybkgwuszlbaqwfichvtneo
+xdmgyjkpruszlbcwwfichvtndo
+xdmgcjkprusyibaqwfichvtneo
+xdmgyjktruszlbwqwficuvtneo
+xdmgxjkpruszlbaqyfichvtnvo
+xdmgytkpruszlbaqwficuvtnlo
+xdmgydkpruszlbaqwfijhvtnjo
+xfmgyjkmruszlbaqwfichvtnes
+xdmgyrktruszlraqwfichvtneo
+xdmgyjkihuszlbaqdfichvtneo
+hdmgyjkpruszeiaqwfichvtneo
+xdmzyjkpruszlbaqwgichvtnxo
+xdmgyjknquszlbpqwfichvtneo
+idmgyjrpruszlbtqwfichvtneo
+xkmgyjkpruuzlbaqwfichvfneo
+xdmgyjkpruszlfaqwficnvtner
+xdmgyjkpruszlbpqwficwvteeo
+xdmgyjkpwuszlbiqwfhchvtneo
+xdmgyjkpruszwbaqwfichrtnbo
+xdpgyjkprusblbaqwfgchvtneo
+xdmryjkcruszlbaqwfichvtnee
+xwmgylkpruszlbaqwfcchvtneo
+xdmgyjkpruszflaqwfixhvtneo
+xdmgyjkmruszloaqwfichvteeo
+xvmgrjkpruszlbaqwfichvsneo
+xdmvyjkprusmlbaqwfichvtnes
+xdmgyjkpruszlbaqwfichkgbeo
+xdmgyikpruxzlbaqwfichvtnei
+xdmgyjkprugzlbaqhfichvtveo
+xdmgyjkpruszlbaqjaichftneo
+xdmzijkpruszlbaqwwichvtneo
+xdmgyjkprsszlbaqwfihhvlneo
+xdmgyjkprusqlwaqzfichvtneo
+ximgyjkpruszlbawwfichvtnen
+xsmgyjzpruszlbaqwfichvaneo
+xdmgyjkpruszlcaoyfichvtneo
+xdmgyjkprusmlbaqvnichvtneo
+xdmgyjkvruszmbaqwfichvtueo
+xdmgyjppuuszleaqwfichvtneo
+xddgyjkprubzlbaqwfichvaneo
+xdmgwjkpruszebaswfichvtneo
+xdogyjkpruszlblqwfichvdneo
+xdkgyjgpruszlbaqwfizhvtneo
+xdvgyjkpruszlbdqwfichvtqeo
+xdmgyjlpruszlbapwficgvtneo
+xdmgyjkpruszlbaqofickvtngo
+xdmgyjkprqszliaywfichvtneo
+xdqgyjkpruszlbcqwficnvtneo
+xdmgdjkpruszlbaqwxichvtseo
+xdmgyjkpruczlbaqwfichdtnfo
+xdmgyjkpruszluaqwficzvtnjo
+xdmgyjkproszlbaqwfacevtneo
+xfmgijkpruszlbrqwfichvtneo
+odmgyjkpluszlbaqwfichvuneo
+xdmgyjkpruszlbaqwwichukneo
+xdmgdjkpruszwbaqwfichvtnet
+xdmgyjkzrusvlbaqwrichvtneo
+xdmgylkprutzlbaqwfichvtnbo
+xdmgyjkpruszsbaqwfijtvtneo
+xdmgyjkproszlbjqwfichntneo
+xdmgyhkpluszlbaqwfichvtnlo
+xdmgyjhprushlbaqwfichvtnzo
+gdmoyjkpruszlbarwfichvtneo
+cdmgyjkpruszlbaqwfcchvtned
+xgmgyjkpruszlbaqwfschvtnek
+xdmgyjkprusnlzamwfichvtneo
+xdmgyjkprgszlbaxwfichvuneo
+txmgyjksruszlbaqwfichvtneo
+xdmgyjkprusbbbpqwfichvtneo
+xdmoyjkpruszlbaqwfighvtxeo
+xdmgyjkpruslhbaqwfichptneo
+xdmgzjkpruszlbaqwffcmvtneo
+xdmgyjkiruszlbaqgficuvtneo
+vdbgyjkpruszlbaqwfichvtnek
+xdmgyjspruszlbaqwfochvtney
+xdmgyjkpruszibaqwfivhvteeo
+xdmgyjkpruszfbaqwficbvtgeo
+xdmgyjkprystlbaqwxichvtneo
+xdmfyjkpryszlxaqwfichvtneo
+xdmgyjgpruspybaqwfichvtneo
+xdmgyjklruszlbjqwdichvtneo
+xdmgyjkzruszltaqwfichvtnek
+xdmgqjkpruszlzaqwfichvtneh
+xdmgyjhnruszlbaqwficqvtneo
+xdmgyjkproszlbaqweichvtnez
+xdmgyjkprurzlbaawfichytneo
+xdmgyfkpruszlbaqwfschutneo
+xdmnyjkpruszlbaawjichvtneo
+xdmgyjkpybszlbaqwfichvwneo
+xdmgtjkhruszlbaqwfichatneo
+xamgyjkprurzlbaqwfichvaneo
+xdmgyjkpruszlbaqwgichvtnqv
+ndmgyjkpruszlsaqwfuchvtneo
+xdmgygkpgusrlbaqwfichvtneo
+xdmgyjkpruszfbaqwfichvtnmy
+xdmgyjkprupflbaqwfichvjneo
+ndmgyjkpruszlbagwfichvtnxo
+xdmgyjkpruszlbafwfilhvcneo
+xdmgyjkpruszlbaqwfichvjsea
+xebgyjkpruszlbaqafichvtneo
+xdmkyjdpruszlbaqwfichvtnei
+xomgyjkprufzlbaqwfochvtneo
+xdmgyjkprfsllbaqwfiihvtneo
+xdmyyjkpruszebaqwficmvtneo
+xdmnyjkpruczlbarwfichvtneo
+xdmgyjkpruszcbaqwbichvtneg
+xdmgxjkpluszlbapwfichvtneo
+xgrlyjkpruszlbaqwfichvtneo
+xdmgyjkpruszlraqwxcchvtneo
+xdmhyjupruszlbaqafichvtneo
+xdmgnjkpruszlbkqwfjchvtneo
+xdmgyjkpruszlwaqwfichvtndg
+xdmgfjkpruvqlbaqwfichvtneo
+xdmgejkptuszlbdqwfichvtneo
+xlmgyjkpruszlnaqwfochvtneo
+xdmgcjkpruszlbaqwfiqhvaneo
+xdmgyjupruyzlbaywfichvtneo
+gdmgyjkpruyzlbaqwficevtneo
+xdmgyjkaruazlbapwfichvtneo
+xsmiyjkpruszlbaqwfichvtveo
+xdmiyjkprukzlbaqwfichvtnea
+xdbgmjkxruszlbaqwfichvtneo
+xdmgyjkpruskvbaqwfichdtneo
+xdmgyjkprusznbaqwficshtneo
+xdmgyjkprusrlbaqwfzchetneo
+xdmgyrkpruszzbaqwfichvtned
+xdmgyjkprusolbacwmichvtneo
+xdmgypkpruszlbaqwfichvtmgo
+xdmgyjkprumzlbhqwfichttneo
+xdmgydkprusglbaqwfichvtnei
+xdmuyjkpruszlbpqwfichvtyeo
+xdmtymkprusslbaqwfichvtneo
+xdmgyjjprkszlbaqwfqchvtneo
+xdmgvjdpruszlbaqwfichgtneo
+xdtgyjkpruwzlbaqwfjchvtneo
+xdmgyjkpruszlbafseichvtneo
+xdmgvjkpruszlraawfichvtneo
+xdmgyukprgszlbatwfichvtneo
+xhmgyjkpruszliaqwnichvtneo
+xdmgyjspruszlbwqyfichvtneo
+xdmgyjkjruszlqaqwfichvtnvo
+xdmgyjkiruszlbnqwfichmtneo
+ximgyjkpruszlbaqwfvcevtneo
+xdmdyjkpruszlbaqwsithvtneo
+ndmgyjkpruszlbaqwfilhatneo
+xdmgyjkpruszlbaqwfinhvcnez
+xdmgypkpsuszlbajwfichvtneo
+xdpgmjkpluszlbaqwfichvtneo
+xdmgyjnprupzlbaqwfichvtnel
+xbmgyjkprmszlfaqwfichvtneo
+xdmgyjkpausllbaqwfichvtseo
+xdmgyjkpruszlbaqwfqchttnes
+xgmgyjkpruszlbaxwfichvtneb
+xdmgyjkpruszabqqwfichvineo
+xdmgpjkpquszlbaqwfichvdneo
+xdmgyjkeruszlbaqdficbvtneo
+xdmaujkpruszlbaqwfichvteeo
+xdmgyjkpruszlbaqwrirhvtnev
+xdmgyjkpsugzllaqwfichvtneo
+xdmgyjkpruszlbaqwfichctnlm
+xdmeyjkpruszlbacwfiwhvtneo
+xdmgyjkpiuhzlbaqwfijhvtneo
+xdmgyjkpruszlbmqhfiohvtneo
+xdegyjkpbuszlbbqwfichvtneo
+xdmggxkpruszlbaqwfirhvtneo
+xdmgojkpruszlbaqvfichvtteo
+xdmgyjhtruszlbaqwmichvtneo
+rdmgyjkpruszlbaqwfichvthek
+xdlgyjqpruszlbaqwfbchvtneo
+xdmgyjspriszlbavwfichvtneo
+rdkgyjkpruszlbaqwfichvtnuo
+tdmgyjkuruszlbaqwfichvtnev
+xdmgyjkpxuszlbaqwfkchvtnso
+xdegyjkpruszlbbqxfichvtneo
+xdmgyjkpruszlbaqwficpvtket
+xdmgyjkpruszliaqwfnchvtnec
+xdmgyjkpreszlbaqwficdvtdeo
+rdmgyjkpruszlbaywfychvtneo
+xdmgywkpruszlbaqwficrvtaeo
+xdmgyjkpruszlbanwflchvoneo
+xdmgyjkpruyzlbaqufychvtneo
+symgyjkpruszlbaqwfichvtqeo
+xdmgyjkpruszlbaqwfichvbzqo
+xzfgyjkpruszlbaqwfichvtveo
+udmgyjepruszlbaqwfichbtneo
+xhmgyjkpruszlbaqwfjchvtnef
+xdhgyjkpruszlbaqaftchvtneo
+xdmzyjkjruszlbaqwfichvtnwo
+xdmgyjepruszlbaqwffchvtnef
+xdmgyjkprurzlbaqwfikhvtneq
+xomoyjkpruszkbaqwfichvtneo
+xdmgyjkpiuszubaqwfichktneo
+xdmgyjkprusdlbaqwhihhvtneo
+xdmgyjkpruszlbaqwwirhvxneo
+xdmgyjkpruszlbaqwficgitzeo
+xdmgyjlpruszlbaqwfichpjneo
+xjmgyjkpxuszlbaqwfichatneo
+xdmgylkpruszlbaqwfiehvtnez
+xdmgbjkpruszmbaqwfihhvtneo
+xdmgyjkprubzlwaqwfichvtxeo
+xdmgyjhlrustlbaqwfichvtneo
+xdmmyjkpruszlbaqwfdchitneo
+xdmgyjkpruszlbaqwoichhtbeo
+xdzgyjkprvszlcaqwfichvtneo
+ndmgyjkpruszlbaqwficavxneo
+xdmgyjfpruszxbaqzfichvtneo
+xdmgyjkpeuszlbaqzficdvtneo
+xdmgyjkpruszlbmqffidhvtneo
+xdnvyjkpruszlbafwfichvtneo
+xdygyjkpruszlbljwfichvtneo
+xdigyjkpruszlbeqwfuchvtneo
+xdmgyjkpruszlbpzwfichvteeo
+bdmgyjbpruszldaqwfichvtneo
+xdmgyjkprrszlbaqmpichvtneo
+idmgyjkpruszlbaqyfichvtkeo
+xdmgyjkmrqsclbaqwfichvtneo
+xdmgyjkpruazlbeqwfichvtxeo
+ddmgyjkpruszlbsqwfichotneo
+xdmgyqkpruszjbaqwfxchvtneo
+xdmnyjkpruozlbaqwfichvtreo
+edmgyjkpruszlbuqwwichvtneo
+xdmgyjkprmshlbaqwfichctneo
+xdmgyjkpruszlbaqwffghotneo
+xdmcyjkprfszlbaqnfichvtneo
+xdmgyjypruszhbaqwficyvtneo
+xdmgyjkprzszlyaqwficmvtneo
+xlmgyjkprzszlbaqwficyvtneo
+xdmgyjkprutulbaqwfithvtneo
+xdygyjkpruszlbpqwfichvpneo
+xdmgsjkpoumzlbaqwfichvtneo
+xdmgyjkpyuszlbaqdfnchvtneo
+xdxgyjkpruszlbaqwfizhvtnjo
+xdmgyjkpruszlbaqwfschvkndo
+xdmgpjkprnszlcaqwfichvtneo
+xhmgyjkpruszlbaqwficgvtnet
+xdmgyjkpruswlbaqwfichvtqer
+ddmgyjkprcszlbaqwfichqtneo
+xdmgyjkpruhhlbaqwpichvtneo
+xdmgyjkeraszlbaqwfichvtnso
+nomgyjkpruszlbaqwficavxneo
+xdmgyjkprdszlbaqwfobhvtneo
+xdmgyjkprgszlbaqwfichvtdao
+xomgyjspruswlbaqwfichvtneo
+xdzgyjkpruszlbaqwficwvpneo
+admgejkpruszlbaqwfimhvtneo
+xdtgyjkpruszlmaqwfiqhvtneo
+xdmgymkprusqlbaqwtichvtneo
+xdmgyjkpluszlbaqwfidhvtnea
+ztmgyjjpruszlbaqwfichvtneo";
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemOne();
+        }
+
+        static void ProblemOne()
+        {
+            List<Step> steps = ParseInput(example);
+            var firstSteps = FindFirstSteps(steps);
+            Step nextStep = SelectNextStep(firstSteps);
+            string order = "";
+            while (nextStep != null)
+            {
+                order += nextStep.StepName;
+                nextStep.Done = true;
+                
+                nextStep = SelectNextStep(firstSteps);
+            }
+        }
+        
+        static Step SelectNextStep(List<Step> steps)
+        {
+            return FindPosibleSteps(steps).OrderBy(i => i.StepName).FirstOrDefault();
+
+        }
+
+        static List<Step> FindPosibleSteps(List<Step> steps)
+        {
+            List<Step> result = new List<Step>();
+            foreach (Step s in steps)
+            {
+                if (!s.Done)
+                {
+                    result.Add(s);
+                }
+                else
+                {
+                    result.AddRange(FindPosibleSteps(s.CompleteFirstSteps));
+                }
+            }
+
+            return result;
+        }
+
+        static List<Step> ParseInput(string input)
+        {
+            Dictionary<string, Step> steps = new Dictionary<string, Step>();
+            //for (int i = 0; i < 26; i++)
+            //{
+            //    string s = ((char)(65 + i)).ToString();
+            //    steps[s] = new Step(){StepName = s};
+            //}
+
+            var split = input.Split(new string[]{"\r\n"}, StringSplitOptions.None).ToList();
+            List<(string mustFinish, string canStart)> list = new List<(string mustFinish, string canStart)>();
+            foreach (string s in split)
+            {
+                string mustFinish = s[5].ToString();
+                string canStart = s[36].ToString();
+                list.Add((mustFinish, canStart));
+
+                if (!steps.ContainsKey(mustFinish))
+                {
+                    steps[mustFinish] = new Step(){StepName = mustFinish };
+                }
+
+                if (!steps.ContainsKey(canStart))
+                {
+                    steps[canStart] = new Step() { StepName = canStart };
+                }
+            }
+
+            foreach (var s in list)
+            {
+                steps[s.canStart].CompleteFirstSteps.Add(steps[s.mustFinish]);
+            }
+
+            List<Step> result = new List<Step>();
+            foreach (var s in steps)
+            {
+                result.Add(s.Value);
+            }
+
+            return result;
+        }
+
+
+        static List<Step> FindFirstSteps(List<Step> steps)
+        {
+            List<Step> result = new List<Step>();
+            foreach (Step s in steps)
+            {
+                if (!IsContainedInOtherStep(s, steps))
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+
+        static bool IsContainedInOtherStep(Step step, List<Step> steps)
+        {
+            foreach (Step s in steps)
+            {
+                if (s.CompleteFirstSteps.Contains(step))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public class Step
+        {
+            public bool Done = false;
+            public string StepName;
+            public List<Step> CompleteFirstSteps = new List<Step>();
+
+            public override string ToString()
+            {
+                return $"{StepName} {CompleteFirstSteps.Count}";
+            }
+        }
+
+
+        private static string example = @"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+
+        private static string input = @"Step P must be finished before step O can begin.
+Step H must be finished before step X can begin.
+Step M must be finished before step Q can begin.
+Step E must be finished before step U can begin.
+Step G must be finished before step O can begin.
+Step W must be finished before step F can begin.
+Step O must be finished before step F can begin.
+Step B must be finished before step X can begin.
+Step F must be finished before step C can begin.
+Step A must be finished before step L can begin.
+Step C must be finished before step D can begin.
+Step D must be finished before step Y can begin.
+Step V must be finished before step R can begin.
+Step I must be finished before step Y can begin.
+Step X must be finished before step K can begin.
+Step T must be finished before step S can begin.
+Step Y must be finished before step J can begin.
+Step Z must be finished before step R can begin.
+Step R must be finished before step K can begin.
+Step K must be finished before step N can begin.
+Step U must be finished before step N can begin.
+Step Q must be finished before step N can begin.
+Step N must be finished before step J can begin.
+Step S must be finished before step J can begin.
+Step L must be finished before step J can begin.
+Step A must be finished before step C can begin.
+Step S must be finished before step L can begin.
+Step X must be finished before step S can begin.
+Step T must be finished before step J can begin.
+Step B must be finished before step C can begin.
+Step G must be finished before step N can begin.
+Step M must be finished before step O can begin.
+Step Y must be finished before step K can begin.
+Step B must be finished before step Y can begin.
+Step Y must be finished before step U can begin.
+Step F must be finished before step J can begin.
+Step A must be finished before step N can begin.
+Step W must be finished before step Y can begin.
+Step C must be finished before step R can begin.
+Step Q must be finished before step J can begin.
+Step O must be finished before step L can begin.
+Step Q must be finished before step S can begin.
+Step H must be finished before step E can begin.
+Step N must be finished before step S can begin.
+Step A must be finished before step T can begin.
+Step C must be finished before step K can begin.
+Step Z must be finished before step J can begin.
+Step U must be finished before step Q can begin.
+Step B must be finished before step F can begin.
+Step W must be finished before step X can begin.
+Step H must be finished before step Q can begin.
+Step B must be finished before step V can begin.
+Step Z must be finished before step U can begin.
+Step O must be finished before step A can begin.
+Step C must be finished before step I can begin.
+Step I must be finished before step T can begin.
+Step E must be finished before step D can begin.
+Step V must be finished before step S can begin.
+Step F must be finished before step V can begin.
+Step C must be finished before step S can begin.
+Step I must be finished before step U can begin.
+Step F must be finished before step Z can begin.
+Step A must be finished before step X can begin.
+Step C must be finished before step N can begin.
+Step G must be finished before step F can begin.
+Step O must be finished before step R can begin.
+Step V must be finished before step X can begin.
+Step E must be finished before step A can begin.
+Step K must be finished before step Q can begin.
+Step Z must be finished before step K can begin.
+Step T must be finished before step K can begin.
+Step Y must be finished before step Z can begin.
+Step W must be finished before step B can begin.
+Step E must be finished before step V can begin.
+Step W must be finished before step J can begin.
+Step I must be finished before step S can begin.
+Step H must be finished before step L can begin.
+Step G must be finished before step I can begin.
+Step X must be finished before step L can begin.
+Step H must be finished before step G can begin.
+Step H must be finished before step Z can begin.
+Step H must be finished before step N can begin.
+Step D must be finished before step I can begin.
+Step E must be finished before step J can begin.
+Step X must be finished before step R can begin.
+Step O must be finished before step J can begin.
+Step N must be finished before step L can begin.
+Step X must be finished before step N can begin.
+Step V must be finished before step Q can begin.
+Step P must be finished before step Y can begin.
+Step H must be finished before step U can begin.
+Step X must be finished before step Z can begin.
+Step G must be finished before step Q can begin.
+Step B must be finished before step Q can begin.
+Step Y must be finished before step L can begin.
+Step U must be finished before step J can begin.
+Step W must be finished before step V can begin.
+Step G must be finished before step C can begin.
+Step G must be finished before step B can begin.
+Step O must be finished before step B can begin.
+Step R must be finished before step N can begin.";
+    }
+}
