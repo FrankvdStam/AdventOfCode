a551b7e3f98e2b193fdd177da595f4a0cad69975
FrankvdStam <frank_stam5@hotmail.com>
2019-02-13 12:31:16 +0100

    day 2 done

--- 2016/Day02/Program.cs
+++ 2016/Day02/Program.cs
@@ -10,33 +10,131 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            ParseInput(Example);
-            ProblemOne();
-            //ProblemTwo();
+            //ProblemOne(Input);
+            ProblemTwo(Input);
         }
 
-        static void ProblemOne()
+        static void ProblemOne(string input)
         {
+            List<int> code = new List<int>();
+            int[,] keypad = new int[,]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+            };
 
-        }
+            //Start at 5
+            int x = 1;
+            int y = 1;
 
-        static void ProblemTwo()
-        {
-
-        }
-
-        static void ParseInput(string input)
-        {
             var lines = input.Split(new string[] { "\r\n" }, StringSplitOptions.None);
             foreach (var line in lines)
             {
-                var bits = line.Split(' ');
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (y - 1 >= 0)
+                            {
+                                y--;
+                            }
+                            break;
+                        case 'D':
+                            if (y + 1 < 3)
+                            {
+                                y++;
+                            }
+                            break;
+                        case 'L':
+                            if (x - 1 >= 0)
+                            {
+                                x--;
+                            }
+                            break;
+                        case 'R':
+                            if (x + 1 < 3)
+                            {
+                                x++;
+                            }
+                            break;
+                    }
+                }
+                //End of sequence: press the key.
+                code.Add(keypad[y, x]);
+                //x = 1;
+                //y = 1;
+            }
+        }
+
+        static void ProblemTwo(string input)
+        {
+            List<char> code = new List<char>();
+            char[,] keypad = new char[,]
+            {
+                {'0', '0', '1', '0', '0'},
+                {'0', '2', '3', '4', '0'},
+                {'5', '6', '7', '8', '9'},
+                {'0', 'A', 'B', 'C', '0'},
+                {'0', '0', 'D', '0', '0'},
+            };
+
+            //Start at 5
+            int x = 0;
+            int y = 2;
+
+            var lines = input.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            foreach (var line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (y - 1 >= 0 && keypad[y-1, x] != '0')
+                            {
+                                y--;
+                            }
+                            break;
+                        case 'D':
+                            if (y + 1 < 5 && keypad[y+1, x] != '0')
+                            {
+                                y++;
+                            }
+                            break;
+                        case 'L':
+                            if (x - 1 >= 0 && keypad[y, x-1] != '0')
+                            {
+                                x--;
+                            }
+                            break;
+                        case 'R':
+                            if (x + 1 < 5 && keypad[y, x+1] != '0')
+                            {
+                                x++;
+                            }
+                            break;
+                    }
+                }
+                //End of sequence: press the key.
+                code.Add(keypad[y, x]);
+                //x = 1;
+                //y = 1;
             }
         }
 
 
-        private static string Input = @"";
-        private static string Example = @"";
+        private static string Input = @"LRRLLLRDRURUDLRDDURULRULLDLRRLRLDULUDDDDLLRRLDUUDULDRURRLDULRRULDLRDUDLRLLLULDUURRRRURURULURRULRURDLULURDRDURDRLRRUUDRULLLLLDRULDDLLRDLURRLDUURDLRLUDLDUDLURLRLDRLUDUULRRRUUULLRDURUDRUDRDRLLDLDDDLDLRRULDUUDULRUDDRLLURDDRLDDUDLLLLULRDDUDDUUULRULUULRLLDULUDLLLLURRLDLUDLDDLDRLRRDRDUDDDLLLLLRRLLRLUDLULLDLDDRRUDDRLRDDURRDULLLURLRDLRRLRDLDURLDDULLLDRRURDULUDUDLLLDDDLLRLDDDLLRRLLURUULULDDDUDULUUURRUUDLDULULDRDDLURURDLDLULDUDUDDDDD
+RUURUDRDUULRDDLRLLLULLDDUDRDURDLRUULLLLUDUDRRUDUULRRUUDDURDDDLLLLRRUURULULLUDDLRDUDULRURRDRDLDLDUULUULUDDLUDRLULRUDRDDDLRRUUDRRLULUULDULDDLRLURDRLURRRRULDDRLDLLLRULLDURRLUDULDRDUDRLRLULRURDDRLUDLRURDDRDULUDLDLLLDRLRUDLLLLLDUDRDUURUDDUDLDLDUDLLDLRRDLULLURLDDUDDRDUDLDDUULDRLURRDLDLLUUDLDLURRLDRDDLLDLRLULUDRDLLLDRLRLLLDRUULUDLLURDLLUURUDURDDRDRDDUDDRRLLUULRRDRULRURRULLDDDUDULDDRULRLDURLUDULDLDDDLRULLULULUDLDDRDLRDRDLDULRRLRLRLLLLLDDDRDDULRDULRRLDLUDDDDLUDRLLDLURDLRDLDRDRDURRDUDULLLDLUDLDRLRRDDDRRLRLLULDRLRLLLLDUUURDLLULLUDDRLULRDLDLDURRRUURDUDRDLLLLLLDDDURLDULDRLLDUDRULRRDLDUDRLLUUUDULURRUR
+URRRLRLLDDDRRLDLDLUDRDRDLDUDDDLDRRDRLDULRRDRRDUDRRUUDUUUDLLUURLRDRRURRRRUDRLLLLRRDULRDDRUDLRLUDURRLRLDDRRLUULURLURURUDRULDUUDLULUURRRDDLRDLUDRDLDDDLRUDURRLLRDDRDRLRLLRLRUUDRRLDLUDRURUULDUURDRUULDLLDRDLRDUUDLRLRRLUDRRUULRDDRDLDDULRRRURLRDDRLLLRDRLURDLDRUULDRRRLURURUUUULULRURULRLDDDDLULRLRULDUDDULRUULRRRRRLRLRUDDURLDRRDDULLUULLDLUDDDUURLRRLDULUUDDULDDUULLLRUDLLLRDDDLUUURLDUDRLLLDRRLDDLUDLLDLRRRLDDRUULULUURDDLUR
+UULDRLUULURDRLDULURLUDULDRRDULULUDLLDURRRURDRLRLLRLDDLURRDLUUDLULRDULDRDLULULULDDLURULLULUDDRRULULULRDULRUURRRUDLRLURDRURDRRUDLDDUURDUUDLULDUDDLUUURURLRRDLULURDURRRURURDUURDRRURRDDULRULRRDRRDRUUUUULRLUUUDUUULLRRDRDULRDDULDRRULRLDLLULUUULUUDRDUUUDLLULDDRRDULUURRDUULLUUDRLLDUDLLLURURLUDDLRURRDRLDDURLDLLUURLDUURULLLRURURLULLLUURUUULLDLRDLUDDRRDDUUDLRURDDDRURUURURRRDLUDRLUULDUDLRUUDRLDRRDLDLDLRUDDDDRRDLDDDLLDLULLRUDDUDDDLDDUURLDUDLRDRURULDULULUDRRDLLRURDULDDRRDLUURUUULULRURDUUDLULLURUDDRLDDUDURRDURRUURLDLLDDUUDLLUURDRULLRRUUURRLLDRRDLURRURDULDDDDRDD
+LLRUDRUUDUDLRDRDRRLRDRRUDRDURURRLDDDDLRDURDLRRUDRLLRDDUULRULURRRLRULDUURLRURLRLDUDLLDULULDUUURLRURUDDDDRDDLLURDLDRRUDRLDULLRULULLRURRLLURDLLLRRRRDRULRUDUDUDULUURUUURDDLDRDRUUURLDRULDUDULRLRLULLDURRRRURRRDRULULUDLULDDRLRRULLDURUDDUULRUUURDRRLULRRDLDUDURUUUUUURRUUULURDUUDLLUURDLULUDDLUUULLDURLDRRDDLRRRDRLLDRRLUDRLLLDRUULDUDRDDRDRRRLUDUDRRRLDRLRURDLRULRDUUDRRLLRLUUUUURRURLURDRRUURDRRLULUDULRLLURDLLULDDDLRDULLLUDRLURDDLRURLLRDRDULULDDRDDLDDRUUURDUUUDURRLRDUDLRRLRRRDUULDRDUDRLDLRULDL";
+
+        private static string Example = @"ULL
+RRDDD
+LURDL
+UUUUD";
 
     }
 }
