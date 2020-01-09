d1dba1732b540aa4b5d89ccd197adb9d866fc2f5
FrankvdStam <frank_stam5@hotmail.com>
2020-01-09 14:09:16 +0100

    Turned example test programs into parameterized test.

--- 2019/src/Tests/IntCodeComputerTests.cs
+++ 2019/src/Tests/IntCodeComputerTests.cs
@@ -12,16 +12,49 @@ namespace Tests
     public class IntCodeComputerTests
     {
 
-        [Test]
-        public void ExamplesDay02()
+        #region Examples
+        [TestCase("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50")]
+        [TestCase("1,0,0,0,99", "2,0,0,0,99")]
+        [TestCase("2,3,0,3,99", "2,3,0,6,99")]
+        [TestCase("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [TestCase("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void TestExampleProgram(string input, string result)
         {
+            IntCodeComputer computer = new IntCodeComputer();
 
-            Assert.AreEqual(true, TestProgram("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50"));
-            Assert.AreEqual(true, TestProgram("1,0,0,0,99", "2,0,0,0,99"));
-            Assert.AreEqual(true, TestProgram("2,3,0,3,99", "2,3,0,6,99"));
-            Assert.AreEqual(true, TestProgram("2,4,4,5,99,0", "2,4,4,5,99,9801"));
-            Assert.AreEqual(true, TestProgram("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"));
+            var inputProgram = IntCodeComputer.ParseProgram(input);
+            var resultProgram = IntCodeComputer.ParseProgram(result);
+
+            computer.Program = inputProgram;
+            computer.Run();
+
+            string error = "";
+
+            if (inputProgram.Count != resultProgram.Count)
+            {
+                error += $"input: {input}\r\nresult:{result}\r\n\r\nResulting programs have different sizes: {inputProgram.Count} != {resultProgram.Count}\r\n";
+            }
+
+            for (int i = 0; i < inputProgram.Count; i++)
+            {
+                try//ignore out of bounds exception from previous check
+                {
+
+                    if (inputProgram[i] != resultProgram[i])
+                    {
+                        error += $"address {i}: {inputProgram[i]} != {resultProgram[i]}";
+                    }
+                }
+                catch { }
+            }
+
+            if (error != "")
+            {
+                Assert.Fail(error);
+            }
+            Assert.Pass();
         }
+        #endregion
 
 
         [Test]
@@ -35,31 +68,7 @@ namespace Tests
         }
 
 
-        private bool TestProgram(string input, string result)
-        {
-            IntCodeComputer computer = new IntCodeComputer();
-
-            var inputProgram = IntCodeComputer.ParseProgram(input);
-            var resultProgram = IntCodeComputer.ParseProgram(result);
-
-            computer.Program = inputProgram;
-            computer.Run();
-
-            if (inputProgram.Count != resultProgram.Count)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < inputProgram.Count; i++)
-            {
-                if (inputProgram[i] != resultProgram[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+        
 
     }
 }
