429a88fd1cfcd57883ca0eeac7151ef079558a58
FrankvdStam <frank_stam5@hotmail.com>
2020-01-10 07:32:51 +0100

    Moved day 6 to 7, maybe I forgot to merge day6..

--- 2019/src/Lib/Day06/Day06.cs
+++ 2019/src/Lib/Day06/Day06.cs
@@ -19,7 +19,7 @@ namespace Lib.Day06
 
         public void ProblemOne()
         {
-            var permutations = GetPermutations(new List<int> {0, 1, 2, 3, 4}, 5).ToList();
+            
         }
 
         public void ProblemTwo()
@@ -28,14 +28,6 @@ namespace Lib.Day06
         }
 
 
-        //https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
-        private static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-        {
-            if (length == 1) return list.Select(t => new T[] { t });
-
-            return GetPermutations(list, length - 1)
-                .SelectMany(t => list.Where(e => !t.Contains(e)),
-                    (t1, t2) => t1.Concat(new T[] { t2 }));
-        }
+       
     }
 }
--- 2019/src/Lib/Day07/Day07.cs
+++ 2019/src/Lib/Day07/Day07.cs
@@ -12,12 +12,23 @@ namespace Lib.Day07
 
         public void ProblemOne()
         {
-            throw new NotImplementedException();
+            var permutations = GetPermutations(new List<int> { 0, 1, 2, 3, 4 }, 5).ToList();
         }
 
         public void ProblemTwo()
         {
             throw new NotImplementedException();
+        }
+
+
+        //https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+        private static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
