429fbd76d5f7b2703704e36052a967167c2864af
Frank van der Stam <frank_stam5@hotmail.com>
2020-11-30 22:10:18 +0100

    Made it possible to run all days - some will give errors and the console output is a big mess :)

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -11,29 +11,56 @@ namespace Cli
     {
         static void Main(string[] args)
         {
+            bool runAll = true;
+            int year = 2015;
+            int day = 7;
+
+            Run(runAll, year, day);
+        }
+
+
+
+        static void Run(bool runAll, int year, int day)
+        {
+
+            //Initialize all IDays
             var type = typeof(IDay);
             var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p)).ToList();
 
             List<IDay> days = new List<IDay>();
             foreach (var t in types)
             {
-                try
+                if (t.IsClass)
                 {
                     var obj = (IDay)Activator.CreateInstance(t);
                     days.Add(obj);
                 }
-                catch { }
             }
 
 
-            int year = 2015;
-            int day = 7;
 
-            var activeDay = days.First(i => i.Year == year && i.Day == day);
 
-            activeDay.ProblemOne();
-            activeDay.ProblemTwo();
-            Console.ReadKey();
+            if (runAll)
+            {
+                for (int y = 2019; y <= 2019; y++)
+                {
+                    for (int d = 1; d <= 25; d++)
+                    {
+                        var day_ = days.First(i => i.Year == y && i.Day == d);
+                        day_.ProblemOne();
+                        day_.ProblemTwo();
+                    }
+                   
+                }
+            }
+            else
+            {
+                var activeDay = days.First(i => i.Year == year && i.Day == day);
+
+                activeDay.ProblemOne();
+                activeDay.ProblemTwo();
+                Console.ReadKey();
+            }
         }
     }
 }
