eed13de848f79a9bfda5f8e5d7b63f34653b5ed9
Frank van der Stam <frank_stam5@hotmail.com>
2020-12-06 18:00:51 +0100

    Solved day 6

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -3,8 +3,8 @@ mod year2015;
 mod year2019;
 mod year2020;
 
-use crate::year2020::day05::problem1;
-use crate::year2020::day05::problem2;
+use crate::year2020::day06::problem1;
+use crate::year2020::day06::problem2;
 use std::time::Instant;
 
 
