b8ff2c1205fad134921419dc7b6c99d9a007c899
Frank van der Stam <frank_stam5@hotmail.com>
2020-12-09 14:11:26 +0100

    fixed 2

--- C#/src/Years/Year2016/Day02.cs
+++ C#/src/Years/Year2016/Day02.cs
@@ -12,10 +12,127 @@ namespace Years.Year2016
 
         public void ProblemOne()
         {
+            List<int> code = new List<int>();
+            int[,] keypad = new int[,]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+            };
+
+            //Start at 5
+            int x = 1;
+            int y = 1;
+
+            var lines = Input.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            foreach (var line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (y - 1 >= 0)
+                            {
+                                y--;
+                            }
+                            break;
+                        case 'D':
+                            if (y + 1 < 3)
+                            {
+                                y++;
+                            }
+                            break;
+                        case 'L':
+                            if (x - 1 >= 0)
+                            {
+                                x--;
+                            }
+                            break;
+                        case 'R':
+                            if (x + 1 < 3)
+                            {
+                                x++;
+                            }
+                            break;
+                    }
+                }
+                //End of sequence: press the key.
+                code.Add(keypad[y, x]);
+                //x = 1;
+                //y = 1;
+            }
+            Console.WriteLine(string.Join(null, code));
         }
 
         public void ProblemTwo()
         {
+            List<char> code = new List<char>();
+            char[,] keypad = new char[,]
+            {
+                {'0', '0', '1', '0', '0'},
+                {'0', '2', '3', '4', '0'},
+                {'5', '6', '7', '8', '9'},
+                {'0', 'A', 'B', 'C', '0'},
+                {'0', '0', 'D', '0', '0'},
+            };
+
+            //Start at 5
+            int x = 0;
+            int y = 2;
+
+            var lines = Input.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            foreach (var line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'U':
+                            if (y - 1 >= 0 && keypad[y - 1, x] != '0')
+                            {
+                                y--;
+                            }
+                            break;
+                        case 'D':
+                            if (y + 1 < 5 && keypad[y + 1, x] != '0')
+                            {
+                                y++;
+                            }
+                            break;
+                        case 'L':
+                            if (x - 1 >= 0 && keypad[y, x - 1] != '0')
+                            {
+                                x--;
+                            }
+                            break;
+                        case 'R':
+                            if (x + 1 < 5 && keypad[y, x + 1] != '0')
+                            {
+                                x++;
+                            }
+                            break;
+                    }
+                }
+                //End of sequence: press the key.
+                code.Add(keypad[y, x]);
+                //x = 1;
+                //y = 1;
+            }
+            Console.WriteLine(string.Join(null, code));
         }
+
+
+        private const string Input = @"LRRLLLRDRURUDLRDDURULRULLDLRRLRLDULUDDDDLLRRLDUUDULDRURRLDULRRULDLRDUDLRLLLULDUURRRRURURULURRULRURDLULURDRDURDRLRRUUDRULLLLLDRULDDLLRDLURRLDUURDLRLUDLDUDLURLRLDRLUDUULRRRUUULLRDURUDRUDRDRLLDLDDDLDLRRULDUUDULRUDDRLLURDDRLDDUDLLLLULRDDUDDUUULRULUULRLLDULUDLLLLURRLDLUDLDDLDRLRRDRDUDDDLLLLLRRLLRLUDLULLDLDDRRUDDRLRDDURRDULLLURLRDLRRLRDLDURLDDULLLDRRURDULUDUDLLLDDDLLRLDDDLLRRLLURUULULDDDUDULUUURRUUDLDULULDRDDLURURDLDLULDUDUDDDDD
+RUURUDRDUULRDDLRLLLULLDDUDRDURDLRUULLLLUDUDRRUDUULRRUUDDURDDDLLLLRRUURULULLUDDLRDUDULRURRDRDLDLDUULUULUDDLUDRLULRUDRDDDLRRUUDRRLULUULDULDDLRLURDRLURRRRULDDRLDLLLRULLDURRLUDULDRDUDRLRLULRURDDRLUDLRURDDRDULUDLDLLLDRLRUDLLLLLDUDRDUURUDDUDLDLDUDLLDLRRDLULLURLDDUDDRDUDLDDUULDRLURRDLDLLUUDLDLURRLDRDDLLDLRLULUDRDLLLDRLRLLLDRUULUDLLURDLLUURUDURDDRDRDDUDDRRLLUULRRDRULRURRULLDDDUDULDDRULRLDURLUDULDLDDDLRULLULULUDLDDRDLRDRDLDULRRLRLRLLLLLDDDRDDULRDULRRLDLUDDDDLUDRLLDLURDLRDLDRDRDURRDUDULLLDLUDLDRLRRDDDRRLRLLULDRLRLLLLDUUURDLLULLUDDRLULRDLDLDURRRUURDUDRDLLLLLLDDDURLDULDRLLDUDRULRRDLDUDRLLUUUDULURRUR
+URRRLRLLDDDRRLDLDLUDRDRDLDUDDDLDRRDRLDULRRDRRDUDRRUUDUUUDLLUURLRDRRURRRRUDRLLLLRRDULRDDRUDLRLUDURRLRLDDRRLUULURLURURUDRULDUUDLULUURRRDDLRDLUDRDLDDDLRUDURRLLRDDRDRLRLLRLRUUDRRLDLUDRURUULDUURDRUULDLLDRDLRDUUDLRLRRLUDRRUULRDDRDLDDULRRRURLRDDRLLLRDRLURDLDRUULDRRRLURURUUUULULRURULRLDDDDLULRLRULDUDDULRUULRRRRRLRLRUDDURLDRRDDULLUULLDLUDDDUURLRRLDULUUDDULDDUULLLRUDLLLRDDDLUUURLDUDRLLLDRRLDDLUDLLDLRRRLDDRUULULUURDDLUR
+UULDRLUULURDRLDULURLUDULDRRDULULUDLLDURRRURDRLRLLRLDDLURRDLUUDLULRDULDRDLULULULDDLURULLULUDDRRULULULRDULRUURRRUDLRLURDRURDRRUDLDDUURDUUDLULDUDDLUUURURLRRDLULURDURRRURURDUURDRRURRDDULRULRRDRRDRUUUUULRLUUUDUUULLRRDRDULRDDULDRRULRLDLLULUUULUUDRDUUUDLLULDDRRDULUURRDUULLUUDRLLDUDLLLURURLUDDLRURRDRLDDURLDLLUURLDUURULLLRURURLULLLUURUUULLDLRDLUDDRRDDUUDLRURDDDRURUURURRRDLUDRLUULDUDLRUUDRLDRRDLDLDLRUDDDDRRDLDDDLLDLULLRUDDUDDDLDDUURLDUDLRDRURULDULULUDRRDLLRURDULDDRRDLUURUUULULRURDUUDLULLURUDDRLDDUDURRDURRUURLDLLDDUUDLLUURDRULLRRUUURRLLDRRDLURRURDULDDDDRDD
+LLRUDRUUDUDLRDRDRRLRDRRUDRDURURRLDDDDLRDURDLRRUDRLLRDDUULRULURRRLRULDUURLRURLRLDUDLLDULULDUUURLRURUDDDDRDDLLURDLDRRUDRLDULLRULULLRURRLLURDLLLRRRRDRULRUDUDUDULUURUUURDDLDRDRUUURLDRULDUDULRLRLULLDURRRRURRRDRULULUDLULDDRLRRULLDURUDDUULRUUURDRRLULRRDLDUDURUUUUUURRUUULURDUUDLLUURDLULUDDLUUULLDURLDRRDDLRRRDRLLDRRLUDRLLLDRUULDUDRDDRDRRRLUDUDRRRLDRLRURDLRULRDUUDRRLLRLUUUUURRURLURDRRUURDRRLULUDULRLLURDLLULDDDLRDULLLUDRLURDDLRURLLRDRDULULDDRDDLDDRUUURDUUUDURRLRDUDLRRLRRRDUULDRDUDRLDLRULDL";
+
+        private const string Example = @"ULL
+RRDDD
+LURDL
+UUUUD";
+
     }
 }
