fc36be6597d6ff5511dbdf2a4928fe1dda1003c1
Frank van der Stam <frank_stam5@hotmail.com>
2020-12-09 22:26:13 +0100

    Fixed 6

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -14,7 +14,7 @@ namespace Cli
         {
             bool runAll = false;
             int year = 2016;
-            int day = 20;
+            int day = 6;
 
             Run(runAll, year, day);
         }
