5b05f2bd43ceab34a458d63837efe5050b0cd2bb
Frank van der Stam <frank_stam5@hotmail.com>
2021-01-31 14:34:31 +0100

    Solved 2020 1 1&2

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -13,8 +13,8 @@ namespace Cli
         static void Main(string[] args)
         {
             bool runAll = false;
-            int year = 2016;
-            int day = 22;
+            int year = 2020;
+            int day = 1;
 
             Run(runAll, year, day);
         }
