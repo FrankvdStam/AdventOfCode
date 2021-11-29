acac949a57854bf6f1dd89c3189c679cd59741e2
Frank van der Stam <frank_stam5@hotmail.com>
2021-11-29 15:55:34 +0100

    Generated files for 2021

---
+++ C#/src/Years/Year2021/Day04.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Years.Utils;
+
+namespace Years.Year2021
+{
+    public class Day04 : IDay
+    {
+        public int Day => 4;
+        public int Year => 2021;
+
+        public void ProblemOne()
+        {
+        }
+
+        public void ProblemTwo()
+        {
+        }
+    }
+}
---
+++ C#/src/Years/Year2021/Day21.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Years.Utils;
+
+namespace Years.Year2021
+{
+    public class Day21 : IDay
+    {
+        public int Day => 21;
+        public int Year => 2021;
+
+        public void ProblemOne()
+        {
+        }
+
+        public void ProblemTwo()
+        {
+        }
+    }
+}
