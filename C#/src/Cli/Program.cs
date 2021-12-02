4094865187345d26054009813a36131b9d049adc
Frank van der Stam <frank_stam5@hotmail.com>
2021-12-02 18:58:03 +0100

    Restored 2017-03

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -14,7 +14,7 @@ namespace Cli
         {
             bool runAll = false;
             int year = 2017;
-            int day =2;
+            int day = 3;
 
             Run(runAll, year, day);
         }
