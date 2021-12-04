9912763df172629eb1710f1ce0e72904151e4a71
Frank van der Stam <frank_stam5@hotmail.com>
2021-12-04 16:54:10 +0100

    Removed the last old 2017 code.

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -14,7 +14,7 @@ namespace Cli
         {
             bool runAll = false;
             int year = 2017;
-            int day = 15;
+            int day = 16;
 
             Run(runAll, year, day);
         }
