7a579f2ab455ea488712a6eb206159130864495d
Frank van der Stam <frank_stam5@hotmail.com>
2021-12-06 13:03:34 +0100

    restored 2018-02

--- C#/src/Years/Year2018/Day02.cs
+++ C#/src/Years/Year2018/Day02.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
 using System.Text;
 using Years.Utils;
 
@@ -12,10 +14,363 @@ namespace Years.Year2018
 
         public void ProblemOne()
         {
+            var twoLetterCount = 0;
+            var threeLetterCount = 0;
+
+            foreach (string s in Input.SplitNewLine())
+            {
+                if (HasExactLetterCount(s, 2))
+                {
+                    twoLetterCount++;
+                }
+
+                if (HasExactLetterCount(s, 3))
+                {
+                    threeLetterCount++;
+                }
+            }
+
+            int checksum = twoLetterCount * threeLetterCount;
+            Console.WriteLine(checksum);
         }
 
         public void ProblemTwo()
         {
+            var split = Input.SplitNewLine();
+            var sanitized = new List<string>();
+            foreach (string s in split)
+            {
+                sanitized.Add(s.Replace("\r", ""));
+            }
+
+            for (int i = 0; i < sanitized.Count; i++)
+            {
+                for (int j = 0; j < sanitized.Count; j++)
+                {
+                    //Don't compare with self.
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (HaveOneCharDifference(sanitized[i], sanitized[j]))
+                    {
+                        var first = sanitized[i];
+                        var second = sanitized[j];
+
+                        //Hackity
+                        var charIndex = 0;
+                        for (int index = 0; index < first.Length; index++)
+                        {
+                            if (first[index] != second[index])
+                            {
+                                charIndex = index;
+                                break;
+                            }
+                        }
+                        var sb = new StringBuilder(first);
+                        sb[charIndex] = ' ';
+                        var result = sb.ToString().Replace(" ", "");
+                        Console.WriteLine(result);
+                        return;
+                    }
+                }
+            }
         }
+
+
+        private bool HasExactLetterCount(string input, int letterCount)
+        {
+            var charCount = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (charCount.ContainsKey(input[i]))
+                {
+                    charCount[input[i]]++;
+                }
+                else
+                {
+                    charCount[input[i]] = 1;
+                }
+            }
+
+            return charCount.ContainsValue(letterCount);
+        }
+        
+        private bool HaveOneCharDifference(string first, string second)
+        {
+            bool firstInequallity = true;
+            if (first.Length != second.Length)
+            {
+                Console.WriteLine("length not equal");
+            }
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    if (firstInequallity)
+                    {
+                        firstInequallity = false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        private const string Input = @"xdmgyjkpruszabaqwficevtjeo
+xdmgybkgwuszlbaqwfichvtneo
+xdmgyjkpruszlbcwwfichvtndo
+xdmgcjkprusyibaqwfichvtneo
+xdmgyjktruszlbwqwficuvtneo
+xdmgxjkpruszlbaqyfichvtnvo
+xdmgytkpruszlbaqwficuvtnlo
+xdmgydkpruszlbaqwfijhvtnjo
+xfmgyjkmruszlbaqwfichvtnes
+xdmgyrktruszlraqwfichvtneo
+xdmgyjkihuszlbaqdfichvtneo
+hdmgyjkpruszeiaqwfichvtneo
+xdmzyjkpruszlbaqwgichvtnxo
+xdmgyjknquszlbpqwfichvtneo
+idmgyjrpruszlbtqwfichvtneo
+xkmgyjkpruuzlbaqwfichvfneo
+xdmgyjkpruszlfaqwficnvtner
+xdmgyjkpruszlbpqwficwvteeo
+xdmgyjkpwuszlbiqwfhchvtneo
+xdmgyjkpruszwbaqwfichrtnbo
+xdpgyjkprusblbaqwfgchvtneo
+xdmryjkcruszlbaqwfichvtnee
+xwmgylkpruszlbaqwfcchvtneo
+xdmgyjkpruszflaqwfixhvtneo
+xdmgyjkmruszloaqwfichvteeo
+xvmgrjkpruszlbaqwfichvsneo
+xdmvyjkprusmlbaqwfichvtnes
+xdmgyjkpruszlbaqwfichkgbeo
+xdmgyikpruxzlbaqwfichvtnei
+xdmgyjkprugzlbaqhfichvtveo
+xdmgyjkpruszlbaqjaichftneo
+xdmzijkpruszlbaqwwichvtneo
+xdmgyjkprsszlbaqwfihhvlneo
+xdmgyjkprusqlwaqzfichvtneo
+ximgyjkpruszlbawwfichvtnen
+xsmgyjzpruszlbaqwfichvaneo
+xdmgyjkpruszlcaoyfichvtneo
+xdmgyjkprusmlbaqvnichvtneo
+xdmgyjkvruszmbaqwfichvtueo
+xdmgyjppuuszleaqwfichvtneo
+xddgyjkprubzlbaqwfichvaneo
+xdmgwjkpruszebaswfichvtneo
+xdogyjkpruszlblqwfichvdneo
+xdkgyjgpruszlbaqwfizhvtneo
+xdvgyjkpruszlbdqwfichvtqeo
+xdmgyjlpruszlbapwficgvtneo
+xdmgyjkpruszlbaqofickvtngo
+xdmgyjkprqszliaywfichvtneo
+xdqgyjkpruszlbcqwficnvtneo
+xdmgdjkpruszlbaqwxichvtseo
+xdmgyjkpruczlbaqwfichdtnfo
+xdmgyjkpruszluaqwficzvtnjo
+xdmgyjkproszlbaqwfacevtneo
+xfmgijkpruszlbrqwfichvtneo
+odmgyjkpluszlbaqwfichvuneo
+xdmgyjkpruszlbaqwwichukneo
+xdmgdjkpruszwbaqwfichvtnet
+xdmgyjkzrusvlbaqwrichvtneo
+xdmgylkprutzlbaqwfichvtnbo
+xdmgyjkpruszsbaqwfijtvtneo
+xdmgyjkproszlbjqwfichntneo
+xdmgyhkpluszlbaqwfichvtnlo
+xdmgyjhprushlbaqwfichvtnzo
+gdmoyjkpruszlbarwfichvtneo
+cdmgyjkpruszlbaqwfcchvtned
+xgmgyjkpruszlbaqwfschvtnek
+xdmgyjkprusnlzamwfichvtneo
+xdmgyjkprgszlbaxwfichvuneo
+txmgyjksruszlbaqwfichvtneo
+xdmgyjkprusbbbpqwfichvtneo
+xdmoyjkpruszlbaqwfighvtxeo
+xdmgyjkpruslhbaqwfichptneo
+xdmgzjkpruszlbaqwffcmvtneo
+xdmgyjkiruszlbaqgficuvtneo
+vdbgyjkpruszlbaqwfichvtnek
+xdmgyjspruszlbaqwfochvtney
+xdmgyjkpruszibaqwfivhvteeo
+xdmgyjkpruszfbaqwficbvtgeo
+xdmgyjkprystlbaqwxichvtneo
+xdmfyjkpryszlxaqwfichvtneo
+xdmgyjgpruspybaqwfichvtneo
+xdmgyjklruszlbjqwdichvtneo
+xdmgyjkzruszltaqwfichvtnek
+xdmgqjkpruszlzaqwfichvtneh
+xdmgyjhnruszlbaqwficqvtneo
+xdmgyjkproszlbaqweichvtnez
+xdmgyjkprurzlbaawfichytneo
+xdmgyfkpruszlbaqwfschutneo
+xdmnyjkpruszlbaawjichvtneo
+xdmgyjkpybszlbaqwfichvwneo
+xdmgtjkhruszlbaqwfichatneo
+xamgyjkprurzlbaqwfichvaneo
+xdmgyjkpruszlbaqwgichvtnqv
+ndmgyjkpruszlsaqwfuchvtneo
+xdmgygkpgusrlbaqwfichvtneo
+xdmgyjkpruszfbaqwfichvtnmy
+xdmgyjkprupflbaqwfichvjneo
+ndmgyjkpruszlbagwfichvtnxo
+xdmgyjkpruszlbafwfilhvcneo
+xdmgyjkpruszlbaqwfichvjsea
+xebgyjkpruszlbaqafichvtneo
+xdmkyjdpruszlbaqwfichvtnei
+xomgyjkprufzlbaqwfochvtneo
+xdmgyjkprfsllbaqwfiihvtneo
+xdmyyjkpruszebaqwficmvtneo
+xdmnyjkpruczlbarwfichvtneo
+xdmgyjkpruszcbaqwbichvtneg
+xdmgxjkpluszlbapwfichvtneo
+xgrlyjkpruszlbaqwfichvtneo
+xdmgyjkpruszlraqwxcchvtneo
+xdmhyjupruszlbaqafichvtneo
+xdmgnjkpruszlbkqwfjchvtneo
+xdmgyjkpruszlwaqwfichvtndg
+xdmgfjkpruvqlbaqwfichvtneo
+xdmgejkptuszlbdqwfichvtneo
+xlmgyjkpruszlnaqwfochvtneo
+xdmgcjkpruszlbaqwfiqhvaneo
+xdmgyjupruyzlbaywfichvtneo
+gdmgyjkpruyzlbaqwficevtneo
+xdmgyjkaruazlbapwfichvtneo
+xsmiyjkpruszlbaqwfichvtveo
+xdmiyjkprukzlbaqwfichvtnea
+xdbgmjkxruszlbaqwfichvtneo
+xdmgyjkpruskvbaqwfichdtneo
+xdmgyjkprusznbaqwficshtneo
+xdmgyjkprusrlbaqwfzchetneo
+xdmgyrkpruszzbaqwfichvtned
+xdmgyjkprusolbacwmichvtneo
+xdmgypkpruszlbaqwfichvtmgo
+xdmgyjkprumzlbhqwfichttneo
+xdmgydkprusglbaqwfichvtnei
+xdmuyjkpruszlbpqwfichvtyeo
+xdmtymkprusslbaqwfichvtneo
+xdmgyjjprkszlbaqwfqchvtneo
+xdmgvjdpruszlbaqwfichgtneo
+xdtgyjkpruwzlbaqwfjchvtneo
+xdmgyjkpruszlbafseichvtneo
+xdmgvjkpruszlraawfichvtneo
+xdmgyukprgszlbatwfichvtneo
+xhmgyjkpruszliaqwnichvtneo
+xdmgyjspruszlbwqyfichvtneo
+xdmgyjkjruszlqaqwfichvtnvo
+xdmgyjkiruszlbnqwfichmtneo
+ximgyjkpruszlbaqwfvcevtneo
+xdmdyjkpruszlbaqwsithvtneo
+ndmgyjkpruszlbaqwfilhatneo
+xdmgyjkpruszlbaqwfinhvcnez
+xdmgypkpsuszlbajwfichvtneo
+xdpgmjkpluszlbaqwfichvtneo
+xdmgyjnprupzlbaqwfichvtnel
+xbmgyjkprmszlfaqwfichvtneo
+xdmgyjkpausllbaqwfichvtseo
+xdmgyjkpruszlbaqwfqchttnes
+xgmgyjkpruszlbaxwfichvtneb
+xdmgyjkpruszabqqwfichvineo
+xdmgpjkpquszlbaqwfichvdneo
+xdmgyjkeruszlbaqdficbvtneo
+xdmaujkpruszlbaqwfichvteeo
+xdmgyjkpruszlbaqwrirhvtnev
+xdmgyjkpsugzllaqwfichvtneo
+xdmgyjkpruszlbaqwfichctnlm
+xdmeyjkpruszlbacwfiwhvtneo
+xdmgyjkpiuhzlbaqwfijhvtneo
+xdmgyjkpruszlbmqhfiohvtneo
+xdegyjkpbuszlbbqwfichvtneo
+xdmggxkpruszlbaqwfirhvtneo
+xdmgojkpruszlbaqvfichvtteo
+xdmgyjhtruszlbaqwmichvtneo
+rdmgyjkpruszlbaqwfichvthek
+xdlgyjqpruszlbaqwfbchvtneo
+xdmgyjspriszlbavwfichvtneo
+rdkgyjkpruszlbaqwfichvtnuo
+tdmgyjkuruszlbaqwfichvtnev
+xdmgyjkpxuszlbaqwfkchvtnso
+xdegyjkpruszlbbqxfichvtneo
+xdmgyjkpruszlbaqwficpvtket
+xdmgyjkpruszliaqwfnchvtnec
+xdmgyjkpreszlbaqwficdvtdeo
+rdmgyjkpruszlbaywfychvtneo
+xdmgywkpruszlbaqwficrvtaeo
+xdmgyjkpruszlbanwflchvoneo
+xdmgyjkpruyzlbaqufychvtneo
+symgyjkpruszlbaqwfichvtqeo
+xdmgyjkpruszlbaqwfichvbzqo
+xzfgyjkpruszlbaqwfichvtveo
+udmgyjepruszlbaqwfichbtneo
+xhmgyjkpruszlbaqwfjchvtnef
+xdhgyjkpruszlbaqaftchvtneo
+xdmzyjkjruszlbaqwfichvtnwo
+xdmgyjepruszlbaqwffchvtnef
+xdmgyjkprurzlbaqwfikhvtneq
+xomoyjkpruszkbaqwfichvtneo
+xdmgyjkpiuszubaqwfichktneo
+xdmgyjkprusdlbaqwhihhvtneo
+xdmgyjkpruszlbaqwwirhvxneo
+xdmgyjkpruszlbaqwficgitzeo
+xdmgyjlpruszlbaqwfichpjneo
+xjmgyjkpxuszlbaqwfichatneo
+xdmgylkpruszlbaqwfiehvtnez
+xdmgbjkpruszmbaqwfihhvtneo
+xdmgyjkprubzlwaqwfichvtxeo
+xdmgyjhlrustlbaqwfichvtneo
+xdmmyjkpruszlbaqwfdchitneo
+xdmgyjkpruszlbaqwoichhtbeo
+xdzgyjkprvszlcaqwfichvtneo
+ndmgyjkpruszlbaqwficavxneo
+xdmgyjfpruszxbaqzfichvtneo
+xdmgyjkpeuszlbaqzficdvtneo
+xdmgyjkpruszlbmqffidhvtneo
+xdnvyjkpruszlbafwfichvtneo
+xdygyjkpruszlbljwfichvtneo
+xdigyjkpruszlbeqwfuchvtneo
+xdmgyjkpruszlbpzwfichvteeo
+bdmgyjbpruszldaqwfichvtneo
+xdmgyjkprrszlbaqmpichvtneo
+idmgyjkpruszlbaqyfichvtkeo
+xdmgyjkmrqsclbaqwfichvtneo
+xdmgyjkpruazlbeqwfichvtxeo
+ddmgyjkpruszlbsqwfichotneo
+xdmgyqkpruszjbaqwfxchvtneo
+xdmnyjkpruozlbaqwfichvtreo
+edmgyjkpruszlbuqwwichvtneo
+xdmgyjkprmshlbaqwfichctneo
+xdmgyjkpruszlbaqwffghotneo
+xdmcyjkprfszlbaqnfichvtneo
+xdmgyjypruszhbaqwficyvtneo
+xdmgyjkprzszlyaqwficmvtneo
+xlmgyjkprzszlbaqwficyvtneo
+xdmgyjkprutulbaqwfithvtneo
+xdygyjkpruszlbpqwfichvpneo
+xdmgsjkpoumzlbaqwfichvtneo
+xdmgyjkpyuszlbaqdfnchvtneo
+xdxgyjkpruszlbaqwfizhvtnjo
+xdmgyjkpruszlbaqwfschvkndo
+xdmgpjkprnszlcaqwfichvtneo
+xhmgyjkpruszlbaqwficgvtnet
+xdmgyjkpruswlbaqwfichvtqer
+ddmgyjkprcszlbaqwfichqtneo
+xdmgyjkpruhhlbaqwpichvtneo
+xdmgyjkeraszlbaqwfichvtnso
+nomgyjkpruszlbaqwficavxneo
+xdmgyjkprdszlbaqwfobhvtneo
+xdmgyjkprgszlbaqwfichvtdao
+xomgyjspruswlbaqwfichvtneo
+xdzgyjkpruszlbaqwficwvpneo
+admgejkpruszlbaqwfimhvtneo
+xdtgyjkpruszlmaqwfiqhvtneo
+xdmgymkprusqlbaqwtichvtneo
+xdmgyjkpluszlbaqwfidhvtnea
+ztmgyjjpruszlbaqwfichvtneo";
     }
 }
