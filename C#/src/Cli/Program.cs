b1d10db3f982ef411b92841f30f3be57e63d1bd1
Frank van der Stam <frank_stam5@hotmail.com>
2022-12-04 12:57:55 +0100

    Refactored 2015-16

--- C#/src/Cli/Program.cs
+++ C#/src/Cli/Program.cs
@@ -16,7 +16,7 @@ namespace Cli
         static void Main(string[] args)
         {
             //InputManager.Instance.AppendInput(2022, 04);
-            RunDay(2015, 15);
+            RunDay(2015, 16);
 
             //RunYear(2015);
         }
