9f35aba34eaf1a064cae19b4f2ced892de553d2b
Frank van der Stam <frank_stam5@hotmail.com>
2022-12-06 09:51:12 +0100

    Solved 2022-06

--- C#/src/Years/Year2022/Day06.cs
+++ C#/src/Years/Year2022/Day06.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Text;
 using Years.Utils;
 
@@ -11,10 +13,43 @@ namespace Years.Year2022
 
         public override void ProblemOne()
         {
+            Console.WriteLine(FindDifferentCharsIndex(Input.Replace("\n", string.Empty), 4) + 4);
         }
 
         public override void ProblemTwo()
         {
+            Console.WriteLine(FindDifferentCharsIndex(Input.Replace("\n", string.Empty), 14) + 14);
         }
+
+        private int FindDifferentCharsIndex(string input, int size)
+        {
+            if(input.Length < size)
+            {
+                throw new Exception();
+            }
+
+            for(int i = 0; i < input.Length - size; i++)
+            {
+                var count = new Dictionary<char, int>();
+                for (int j = i; j < i + size; j++)
+                {
+                    if(!count.ContainsKey(input[j]))
+                    {
+                        count[input[j]] = 0;
+                    }
+                    count[input[j]]++;
+                }
+
+                if(count.All(i => i.Value == 1))
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception();
+        }
+
+        private const string Example1 = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        private const string Example5 = @"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
     }
 }
